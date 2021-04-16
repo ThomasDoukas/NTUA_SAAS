e196e4763a593d89d3de38fde48c4a15b551e683
Thomas Doukas <tom.doukas7@gmail.com>
2021-04-16 14:52:45 +0300

    Use TypeORM for PostgreSQL database. Add user CRUD endpoints

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+    
+    @Column()
+    userName: string
+    
+    @Column()
+    firstName: string
+    
+    @Column()
+    lastName: string
+
+    @Column()
+    email: string
+
+    // Will add questions, answers, userType(reward system -> accepted questions)
+}
