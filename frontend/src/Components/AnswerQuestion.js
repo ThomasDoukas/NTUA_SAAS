b0d147b3468d3e0088bff79dfaec3daab905401d
giannispsarr <57642467+giannispsarr@users.noreply.github.com>
2021-04-20 17:29:46 +0300

    Initial frontend with basic routes, signup/login

---
+++ frontend/src/Components/AnswerQuestion.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import './AnswerQuestion.css';
+import { Jumbotron, Col, Image, Container, Row } from 'react-bootstrap';
+
+class AnswerQuestion extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Future Answer Question page</h1>
+            </div>
+        )
+    }
+}
+
+export default AnswerQuestion;
