8736ccdf9d83d78c0ecd9cf28844c7f622ce18a6
giannispsarr <57642467+giannispsarr@users.noreply.github.com>
2021-05-20 15:08:30 +0300

    Added some hard coded questions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import './App.css';
 import Home from './Components/Home';
 import NavBar from './Components/Navbar';
 import SignIn from './Components/SignIn';
@@ -10,17 +9,30 @@ import MyProfile from './Components/MyProfile';
 import ByPeriod from './Components/ByPeriod';
 import ByKeyword from './Components/ByKeyword';
 import { Route } from 'react-router-dom';
+import BrowseQuestions from './Components/BrowseQuestions';
+import Question from './Components/Question';
 
 class App extends React.Component{
 
   state = {
-    loggedin: false
+    loggedin: false,
+    questions: [{title: 'Question 1',
+                body: 'This is the body of the first question. I would like to ask you if you know how to enter the cult. I have been trying for ages but I cant figure out the way.',
+                labels: '#cult, #wena, #indlovu, #enter'}, 
+                {title: 'Question 2',
+                body: 'Now that I have found a way to enter the cult, can you please show me the way out? I have been here for ages and i think I am stuck in this endless pit of destruction.',
+                labels: '#escape, #cult, #help'}, 
+                {title: 'Question 3',
+                body: 'I have figured that there is indeed no way out of the cult. Can you tell me, at least, how can I best serve the Wena Indlovu masters?',
+                labels: '#serve, #worship, #wena, #indlovu'}, 
+                {title: 'Question 4',
+                body: 'Are you done with this elaborate joke?',
+                labels: '#idiot'}]
   }
 
   handleLogIn = (newvalue) => {
     this.setState({ loggedin: newvalue })
     }
-
 
 
   render(){
@@ -32,6 +44,7 @@ class App extends React.Component{
         <Route exact path="/login" render={props => <LogIn {...props} loggedin={this.state.loggedin} onLogIn = {this.handleLogIn}/>}/>
         <Route exact path="/ask" component={AskQuestion} />
         <Route exact path="/answer" component={AnswerQuestion} />
+        <Route exact path="/browse" render={props => <BrowseQuestions {...props} questions={this.state.questions}/>} />
         <Route exact path="/myprofile" render={props => <MyProfile {...props} loggedin={this.state.loggedin} onLogIn = {this.handleLogIn}/>}/>
         <Route exact path="/byperiod" component={ByPeriod} />
         <Route exact path="/bykeyword" component={ByKeyword} />
