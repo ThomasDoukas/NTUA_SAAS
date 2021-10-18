c10e5e27aa39bd1e2917e14c6dcc4cddeeb96713
Thomas Doukas <tom.doukas7@gmail.com>
2021-10-18 18:52:58 +0300

    Remove unnecessary files.

--- backend/src/questions/dto/update-question.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType, OmitType } from '@nestjs/mapped-types';
-import { CreateQuestionDto } from './create-question.dto';
-
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
